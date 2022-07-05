badad3b5ea55523f5a611f2ca0063d55a5215bc3
Fede Liquin PCWIN <josefliquin@gmail.com>
2022-07-05 10:35:36 -0300

    Se actualiza conexión con bases de datos y creacion de modelo

--- proyectovideos/settings.py
+++ proyectovideos/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'proyectovideos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cac_videos',
+        'USER': 'root',
+        'PASSWORD':'cac2022',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+
+    id= models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100,verbose_name='Nombre')
+    descripcion = models.TextField(null=True,verbose_name='Descripcion')
+    imagen = models.ImageField(upload_to='imagenes/',verbose_name='Portada')
